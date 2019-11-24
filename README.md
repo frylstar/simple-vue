896b511cfb3a6e9edbd77748cfa8a7baa5119c2a
shizhongzheng <chengxiaszz@sina.com>
2019-11-25 00:24:20 +0800

    add: basic version1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # simple-vue
 npm 6.4.1
 node 10.15.3
+yarn 1.17.3
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1 class="title" v-if="1">Hello World</h1>
+    <h2>这是简易Vue项目配置23333</h2>
+    <h3>html-webpack-plugin</h3>
+    <h4>VueLoaderPlugin</h4>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+// @titleColor: red;
+@import './theme/base.less';
+.container {
+  .title {
+    color: @titleColor;
+  }
+}
+</style>
